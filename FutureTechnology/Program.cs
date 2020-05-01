eb02903dd17a6d29e540d1b4f538be404735eca3
Amadeusz Wieczorek <amwieczo@microsoft.com>
2020-05-01 10:22:00 -0700

    code

---
+++ FutureTechnology/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FutureTechnology
+{
+    class FutureTechnology
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Provide an argument with the file path of image to transform");
+                return;
+            }
+
+            var legacyProgram = new ImageResizeDemo.Program();
+            legacyProgram.TransformImage(args[0]);
+        }
+    }
+}
---
+++ ImageResizeDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace ImageResizeDemo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var program = new Program();
+            program.TransformImage(@"D:\Untitled.png");
+        }
+
+        public void TransformImage(string path)
+        {
+            var image = Image.Load(path);
+            //image.Mutate();
+            image.Save(path);
+        }
+    }
+}
